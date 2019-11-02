a6ec2e927206579d417f7877356fa1285eb37763
Evana Margain <evana.margain@calmcascade.com>
2019-11-01 19:54:27 -0600

    Initial Commit

---
+++ X-Team_Test_Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  X-Team_Test_Movies
+//
+//  Created by Evana Margain Puig on 01/11/19.
+//  Copyright © 2019 Evana_Margain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ X-Team_Test_MoviesTests/X_Team_Test_MoviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  X_Team_Test_MoviesTests.swift
+//  X-Team_Test_MoviesTests
+//
+//  Created by Evana Margain Puig on 01/11/19.
+//  Copyright © 2019 Evana_Margain. All rights reserved.
+//
+
+import XCTest
+@testable import X_Team_Test_Movies
+
+class X_Team_Test_MoviesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
